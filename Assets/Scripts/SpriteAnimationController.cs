fc943f5ecdfe74a390398509873c091821c14f74
Owen <owen@kudan.eu>
2017-10-14 11:54:41 +0100

    Added sprite animators and sprite controllers.

---
+++ Assets/Scripts/SpriteAnimationController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimationController : MonoBehaviour {
+
+	public SpriteAnimator[] animations;
+
+	public int animationToPlay;
+	int animationCurrentlyPlaying;
+	int numAnimations;
+	// Use this for initialization
+	void Start () {
+		numAnimations = animations.Length;
+		animationCurrentlyPlaying = animationToPlay;
+		animations[animationToPlay].startAnimation();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(animationToPlay < 0 || animationToPlay >= numAnimations){
+			animationToPlay = animationCurrentlyPlaying;
+			return;
+		}
+		if(animationToPlay != animationCurrentlyPlaying){
+			animations[animationCurrentlyPlaying].stopAnimation();
+			animations[animationToPlay].startAnimation();
+			animationCurrentlyPlaying = animationToPlay;
+		}
+	}
+}
---
+++ Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteAnimator : MonoBehaviour {
+
+	// Use this for initialization
+	public string name;
+	SpriteRenderer spriteRenderer;
+	int spriteIndex = 0;
+	int numSprites;
+
+	int indexDirection = 1;
+
+	public Sprite[] sprites;
+	bool isPlaying;
+
+	public float frameTime = 0.1f;
+
+	void Start () {
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		numSprites = sprites.Length;
+	}
+	public void startAnimation(){
+		isPlaying = true;
+		StartCoroutine(animation());
+	}
+	public void stopAnimation(){
+		isPlaying = false;
+		indexDirection = 1;
+		spriteIndex = 0;
+	}
+	IEnumerator animation(){
+		while(isPlaying){
+			yield return new WaitForSecondsRealtime(frameTime);
+			spriteRenderer.sprite = sprites[spriteIndex];
+			if(spriteIndex == numSprites - 1 || (spriteIndex == 0 && indexDirection == -1)){
+				indexDirection *= -1;
+			}
+			spriteIndex += indexDirection;
+		}
+	}
+}
