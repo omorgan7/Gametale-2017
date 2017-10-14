19f547df816bf249b55477d9ada19df1d3c13b0f
Owen <owen@kudan.eu>
2017-10-14 23:24:15 +0100

    Added menu selection for the performances.

---
+++ Assets/Scripts/MenuInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInput : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Enums.Performances selected = Enums.Performances.Flip;
+	// Update is called once per frame
+	void LateUpdate () {
+
+		if(Input.GetButtonDown("Vertical")){
+			int sign;
+			if(Input.GetAxisRaw("Vertical") < 0){
+				sign = -1;
+			}
+			else{
+				sign = 1;
+			}
+			int selectedIndex = ((int) selected + sign);
+			if(selectedIndex == -1){
+				selectedIndex = 2;
+			}
+			if(selectedIndex == 3){
+				selectedIndex = 0;
+			}
+			selected = (Enums.Performances) selectedIndex;
+		}
+	}
+}
---
+++ Assets/Scripts/UIPerformanceSelector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPerformanceSelector : MonoBehaviour {
+
+	MenuInput menuInput;
+	Enums.Performances previousSelection = Enums.Performances.Flip;
+
+	float[] anchorPositions = {0.8f, 0.6f, 0.4f};
+	public RectTransform arrow;
+	// Use this for initialization
+	void Start () {
+		menuInput = gameObject.GetComponent<MenuInput>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if(previousSelection != menuInput.selected){
+			previousSelection = menuInput.selected;
+			Vector2 minAnchors = arrow.anchorMin;
+			Vector2 maxAnchors = arrow.anchorMax;
+			//print((int)previousSelection);
+			arrow.anchorMax = new Vector2(maxAnchors.x,anchorPositions[(int)previousSelection]);
+			arrow.anchorMin = new Vector2(minAnchors.x, arrow.anchorMax.y - 0.2f);
+		}
+	}
+}
