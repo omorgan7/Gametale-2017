98a8f418d500d4767894a927cbe2d5b37f5379ee
Catherine <cat.taylor.taylor@btinternet.com>
2017-10-14 14:42:08 +0100

    Created basic dialogue system for each character

---
+++ Assets/Scripts/DialogueSystemBadger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystemBadger : MonoBehaviour {
+
+	public GameObject speechBubble;
+	private bool isTalking = false; 
+	private float pauseTime = 0.1f; //maybe use deltaTime
+	private GameObject box;
+
+	LoadText loadText  = new LoadText();
+	void Start () {
+		LoadInDialogue(LoadText.scene.b);
+		StartCoroutine( speak(0,4));
+	}
+	
+	void LoadInDialogue(LoadText.scene _scene){
+		switch (_scene){
+			case LoadText.scene.a:
+				loadText.Load("Assets/Character Dialogue/badger/level1.txt", LoadText.characters.badger);
+				break;
+			case LoadText.scene.b:
+				loadText.Load("Assets/Character Dialogue/badger/level2.txt", LoadText.characters.badger);
+				break;
+		}
+
+	}
+	// Update is called once per frame
+	void Update () {
+
+		if (isTalking == true){
+		//speak
+			isTalking = false;
+		}
+		
+	}
+	IEnumerator speak(int startIndex, int EndIndex){
+		box = Instantiate(speechBubble, Vector3.zero, Quaternion.identity); 
+		Text txt = box.transform.GetChild(0).GetChild(0).GetComponent<Text>();
+		for(int i = startIndex; i< EndIndex + 1; ++i){
+			txt.text =loadText.badgerDialogue[i];
+			yield return new WaitForSeconds(2f);
+			
+		}	
+		Destroy(box);	
+	}
+}
---
+++ Assets/Scripts/DialogueSystemMonk.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystemMonk : MonoBehaviour {
+
+	public GameObject speechBubble;
+	private bool isTalking = false; 
+	private float pauseTime = 0.1f; //maybe use deltaTime
+	private GameObject box;
+
+	LoadText loadText  = new LoadText();
+	void Start () {
+		LoadInDialogue(LoadText.scene.b);
+		StartCoroutine( speak(0,4));
+	}
+	
+	void LoadInDialogue(LoadText.scene _scene){
+		switch (_scene){
+			case LoadText.scene.a:
+				loadText.Load("Assets/Character Dialogue/monk/level1.txt", LoadText.characters.monk);
+				break;
+			case LoadText.scene.b:
+				loadText.Load("Assets/Character Dialogue/monk/level2.txt", LoadText.characters.monk);
+				break;
+		}
+
+	}
+	// Update is called once per frame
+	void Update () {
+
+		if (isTalking == true){
+		//speak
+			isTalking = false;
+		}
+		
+	}
+	IEnumerator speak(int startIndex, int EndIndex){
+		box = Instantiate(speechBubble, Vector3.zero, Quaternion.identity); 
+		Text txt = box.transform.GetChild(0).GetChild(0).GetComponent<Text>();
+		for(int i = startIndex; i< EndIndex + 1; ++i){
+			txt.text =loadText.monkDialogue[i];
+			yield return new WaitForSeconds(2f);
+			
+		}	
+		Destroy(box);	
+	}
+}
