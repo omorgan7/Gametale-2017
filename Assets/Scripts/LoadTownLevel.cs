ad775d792e3b018a29bbc51329a6b2a08803b428
Catherine <cat.taylor.taylor@btinternet.com>
2017-10-15 16:38:03 +0100

    Added level loader

---
+++ Assets/Scripts/LoadTownLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadTownLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "door"){
+			 SceneManager.LoadScene("town.scene");
+    
+		}
+	}
+}
