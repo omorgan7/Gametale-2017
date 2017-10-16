fad205f81f3a7696e40bc442d1943858ef720464
Owen <owen@kudan.eu>
2017-10-16 20:11:01 +0100

    Town animation finished.

--- Assets/Scripts/TownAnimator.cs
+++ Assets/Scripts/TownAnimator.cs
@@ -6,6 +6,7 @@ public class TownAnimator : MonoBehaviour {
 
 	// Use this for initialization
 	public GameObject npcContainer;
+	public Transform bunbuku;
 	Transform[] npcs;
 	float height = 1f;
 	void Start () {
@@ -38,7 +39,40 @@ public class TownAnimator : MonoBehaviour {
 		}
 	}
 	IEnumerator bunbukuJumpingRoutine(){
-		yield return null;
+		while(true){
+			yield return new WaitForSecondsRealtime(0.5f);
+			Vector3 initialPosition = bunbuku.transform.position;
+			var spriteController = bunbuku.transform.GetChild(0).GetComponent<SpriteAnimationController>();
+			float elapsedTime = 0f;
+			do{
+				bunbuku.Rotate(new Vector3(0f,0f, 360f*Time.fixedDeltaTime));
+				bunbuku.position = initialPosition;
+				elapsedTime += Time.fixedDeltaTime;
+				yield return new WaitForEndOfFrame();
+			}while(elapsedTime <= 1f);
+			bunbuku.Rotate(new Vector3(0f,0f, -7.2f));
+			bunbuku.position = initialPosition;
+			yield return new WaitForSecondsRealtime(0.5f);
+			elapsedTime = 0f;
+			while(elapsedTime <= 0.5f){
+				bunbuku.position = new Vector3(initialPosition.x, initialPosition.y + Mathf.SmoothStep(0f,height,2f*elapsedTime),initialPosition.z);
+				elapsedTime += Time.deltaTime;
+				yield return new WaitForEndOfFrame();
+			}
+			while(elapsedTime > 0f){
+				bunbuku.position = new Vector3(initialPosition.x, initialPosition.y + Mathf.SmoothStep(0f,height,2f*elapsedTime ),initialPosition.z);
+				elapsedTime -= Time.deltaTime;
+				yield return new WaitForEndOfFrame();
+			}
+			spriteController.nextAnimation = Enums.AnimStates.Idle;
+			yield return new WaitForSecondsRealtime(0.25f);
+			spriteController.nextAnimation = Enums.AnimStates.MoveRight;
+			yield return new WaitForSecondsRealtime(0.25f);
+			spriteController.nextAnimation = Enums.AnimStates.Idle;
+			yield return new WaitForSecondsRealtime(0.25f);
+			spriteController.nextAnimation = Enums.AnimStates.MoveLeft;
+			yield return new WaitForSecondsRealtime(0.25f);
+		}
 	}
 
 }
