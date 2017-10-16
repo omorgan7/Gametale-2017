4a1d5be1dc93ff8626d7458fc284ca14d8760d93
Owen <owen@kudan.eu>
2017-10-16 23:58:23 +0100

    Added monk fixing code.

---
+++ Assets/Scripts/MonkFixer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkFixer : MonoBehaviour {
+
+	// Use this for initialization
+	Vector3 pos;
+	void Start () {
+		pos = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = pos;
+	}
+}
