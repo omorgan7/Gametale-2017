98fc4c4596321d24d50c359ee1f9402994755798
Owen <owen@kudan.eu>
2018-01-14 16:39:32 +0000

    Minor changes.

--- Assets/ResetGame.cs
+++ Assets/ResetGame.cs
@@ -1,27 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ResetGame : MonoBehaviour {
 	public GameObject eventSystem;
-	private Button button;
 	private FadeController fadeController;
-    void Start(){
-        button = gameObject.GetComponent<Button>();
-        button.onClick.AddListener(TaskOnClick);
-		fadeController = eventSystem.GetComponent<FadeController>();
-    }
-	void TaskOnClick(){
-		fadeController.FadeOut();
-		StartCoroutine(loadLevel());
-	}
 
-	IEnumerator loadLevel(){
-		while(!fadeController.isDone){
-			yield return null;
-		}
-		SceneManager.LoadScene("house.scene");
+	void Start(){
+        fadeController = gameObject.GetComponent<FadeController>();
+    }
+	void Update(){
+		if(Input.GetButtonUp("Submit") && !fadeController.isDone){
+            fadeController.FadeOut();
+        }
+        if(fadeController.isDone){
+            SceneManager.LoadScene("splashScreen.scene");
+        }
 	}
 }
