769989f2b0a8bc89636a1a1650bb871230913295
Owen <owen@kudan.eu>
2018-01-14 15:37:47 +0000

    Added animated text.

--- Assets/TextAnimator.cs
+++ Assets/TextAnimator.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class TextAnimator : MonoBehaviour {
 
 	// Use this for initialization
-
 	SpriteRenderer spriteRenderer;
+	public float framerate = 0.1f;
 	void Start () {
-		
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, Mathf.PingPong(Time.time, 0.8f) + 0.2f);
 	}
 }
