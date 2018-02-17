2e64ab2a3bd0a6720d4b5138894796e1be32ba61
Owen <owen@kudan.eu>
2018-02-17 14:57:35 +0000

    Fixed for webgl.

--- Assets/Scripts/URLClicker.cs
+++ Assets/Scripts/URLClicker.cs
@@ -2,10 +2,24 @@
 
 public class URLClicker : MonoBehaviour {
     public void OpenFigurine() {
-        Application.OpenURL("https://sketchfab.com/models/7ffa23f14a9e48f89de9e7656dce0ac7");
+
+		if(Application.platform == RuntimePlatform.WebGLPlayer)
+		{
+			Application.ExternalEval("window.open(\"https://sketchfab.com/models/7ffa23f14a9e48f89de9e7656dce0ac7\",\"_blank\")");
+		}
+		else {
+			Application.OpenURL("https://sketchfab.com/models/7ffa23f14a9e48f89de9e7656dce0ac7");
+		}
+        
     }
 
 	public void OpenGametale() {
-        Application.OpenURL("https://gametale.org/");
+		if(Application.platform == RuntimePlatform.WebGLPlayer)
+		{
+			Application.ExternalEval("window.open(\"https://gametale.org/\",\"_blank\")");
+		}
+		else {
+        	Application.OpenURL("https://gametale.org/");
+		}
     }
 }
