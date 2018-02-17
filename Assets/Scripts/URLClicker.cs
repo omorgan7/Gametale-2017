c639169ca7b8308e77cf300cac244aed5efcae9d
Owen <owen@kudan.eu>
2018-02-17 14:28:40 +0000

    Url functions added.

---
+++ Assets/Scripts/URLClicker.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class URLClicker : MonoBehaviour {
+    public void OpenFigurine() {
+        Application.OpenURL("https://sketchfab.com/models/7ffa23f14a9e48f89de9e7656dce0ac7");
+    }
+
+	public void OpenGametale() {
+        Application.OpenURL("https://gametale.org/");
+    }
+}
